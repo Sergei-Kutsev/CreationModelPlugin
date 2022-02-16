1aabd0fdbe47862f9e471f644d99248eb440d634
Сергей Куцев <sergeikutsev@yandex.ru>
2022-02-16 23:04:26 +0300

    ок

--- CreationModelPlugin/CreationModel.cs
+++ CreationModelPlugin/CreationModel.cs
@@ -1,4 +1,5 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
@@ -44,15 +45,20 @@ namespace CreationModelPlugin
 
             Transaction transaction = new Transaction(doc, "Create wall");
             transaction.Start();
+
             List<Wall> wall = CreateWall(doc, level1, level2); //создали стены
+
             AddDoor(doc, level1, wall[0]);
+
             for (int i = 1; i < 4; i++)
             {
                 AddWindow(doc, level1, wall[i]);
             }
-            
-           
-            
+
+            //AddRoof(doc, level2, wall);
+
+            AddRoofByExtrusion(doc, level2, wall);
+
             transaction.Commit();
 
             return Result.Succeeded;
@@ -67,7 +73,7 @@ namespace CreationModelPlugin
             return listLevel;
         }
 
-        public List <Wall> CreateWall(Document doc, Level level1, Level level2)
+        public List<Wall> CreateWall(Document doc, Level level1, Level level2)
         {
 
             double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
@@ -114,7 +120,6 @@ namespace CreationModelPlugin
             doc.Create.NewFamilyInstance(point, doorType, wall, level1, StructuralType.NonStructural);
         }
 
-
         public void AddWindow(Document doc, Level level1, Wall wall)
         {
             FamilySymbol windowType = new FilteredElementCollector(doc)
@@ -140,5 +145,80 @@ namespace CreationModelPlugin
             sillHeightParameter.Set(UnitUtils.ConvertToInternalUnits(sillHeight, UnitTypeId.Millimeters));
         }
 
+        public void AddRoof(Document doc, Level level2, List<Wall> wall)
+        {
+            RoofType roofType = new FilteredElementCollector(doc)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>() //коллекция преобразовывается в коллекцию экземпляров RoofType
+        .Where(XYZ => XYZ.Name.Equals("Cold Roof - Concrete"))
+        .Where(x => x.FamilyName.Equals("Basic Roof"))
+        .FirstOrDefault();
+
+            double wallThikness = wall[0].Width;
+            double dt = wallThikness / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dt, -dt, 0));
+            points.Add(new XYZ(dt, -dt, 0));
+            points.Add(new XYZ(dt, dt, 0));
+            points.Add(new XYZ(-dt, dt, 0));
+            points.Add(new XYZ(-dt, -dt, 0));
+
+            Application application = doc.Application;
+            CurveArray footPrint = application.Create.NewCurveArray(); // footptint отпечаток границы дома, по которой будет автоматически построена крыша
+
+            for (int i = 0; i < 4; i++)
+            {
+                LocationCurve curve = wall[i].Location as LocationCurve;//у каждой стены берем свойство location и преобразуем его в LocationCurve, чтобы получить кривую
+                XYZ p1 = curve.Curve.GetEndPoint(0);
+                XYZ p2 = curve.Curve.GetEndPoint(1);
+                Line line = Line.CreateBound(p1 + points[i], p2 + points[i + 1]);
+                footPrint.Append(line);
+            }
+            ModelCurveArray footPrintToModelCurveMapping = new ModelCurveArray();
+            FootPrintRoof footprintRoof = doc.Create.NewFootPrintRoof(footPrint, level2, roofType, out footPrintToModelCurveMapping);
+
+            foreach (ModelCurve a in footPrintToModelCurveMapping)
+            {
+                footprintRoof.set_DefinesSlope(a, true);
+                footprintRoof.set_SlopeAngle(a, 0.5);
+            }
+        }
+
+        private void AddRoofByExtrusion(Document doc, Level level2, List<Wall> wall)
+        {
+            RoofType roofType = new FilteredElementCollector(doc)
+                .OfClass(typeof(RoofType))
+                .OfType<RoofType>() //коллекция преобразовывается в коллекцию экземпляров RoofType
+        .Where(XYZ => XYZ.Name.Equals("Cold Roof - Concrete"))
+        .Where(x => x.FamilyName.Equals("Basic Roof"))
+        .FirstOrDefault();
+
+            CurveArray curveArray = new CurveArray();
+
+            LocationCurve curve0 = wall[0].Location as LocationCurve;//у каждой стены берем свойство location и преобразуем его в LocationCurve, чтобы получить кривую
+            LocationCurve curve1 = wall[1].Location as LocationCurve;
+            LocationCurve curve2 = wall[3].Location as LocationCurve;
+
+            double elevation = level2.Elevation;
+
+            XYZ p1 = curve0.Curve.GetEndPoint(0);
+            XYZ p2 = curve1.Curve.GetEndPoint(1);
+            XYZ p3 = curve2.Curve.GetEndPoint(0);
+            XYZ p4 = new XYZ(p1.X, p1.Y - p2.Y, 10);
+
+            double halfOfWallThikness = wall[0].Width / 2;
+
+            curveArray.Append(Line.CreateBound(new XYZ(p1.X, p1.Y - halfOfWallThikness, p1.Z + elevation), new XYZ(p2.X, p1.Y+p2.Y, 18)));
+            curveArray.Append(Line.CreateBound(new XYZ(p2.X, p1.Y + p2.Y, 18), new XYZ(p2.X, p2.Y + halfOfWallThikness, p2.Z + elevation)));
+
+            ReferencePlane plane = doc.Create.NewReferencePlane(new XYZ(0, 0, 0), new XYZ(0, 0, 20), new XYZ(0, 20, 0), doc.ActiveView);
+
+            double startExtrusion = p2.X + halfOfWallThikness;
+            double endExtrusion = p3.X - halfOfWallThikness;
+
+            var roof = doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, startExtrusion, endExtrusion);
+            roof.EaveCuts = EaveCutterType.TwoCutPlumb;
+        }
     }
 }
